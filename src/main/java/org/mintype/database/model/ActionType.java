3cb0dca5a1e834fd9df14c71833581af4d6aa73d
Shahrian Masud <mintype.code@gmail.com>
2026-07-12 13:07:07 -0400

    Add log-entry model

---
+++ src/main/java/org/mintype/database/model/ActionType.java
@@ -0,0 +1,14 @@
+package org.mintype.database.model;
+
+public enum ActionType {
+    BLOCK_BREAK,
+    BLOCK_PLACE,
+    CONTAINER_OPEN,
+    CONTAINER_CHANGE,
+    ITEM_INSERT,
+    ITEM_REMOVE,
+    ENTITY_DAMAGE,
+    ENTITY_DEATH,
+    EXPLOSION,
+    FIRE_SPREAD
+}
---
+++ src/main/java/org/mintype/database/model/LogEntry.java
@@ -0,0 +1,15 @@
+package org.mintype.database.model;
+
+import java.util.UUID;
+
+public record LogEntry(
+        long id,
+        UUID player,
+        ActionType action,
+        String world,
+        int x,
+        int y,
+        int z,
+        long timestamp,
+        String data
+) {}
